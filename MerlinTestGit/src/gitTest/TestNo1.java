4bc809006ecdbbef494134d975117e810f4dea46
NEXT <NEXT@Merlin>
2023-01-27 20:25:33 +0700

    Test Commit 02

--- MerlinTestGit/src/gitTest/TestNo1.java
+++ MerlinTestGit/src/gitTest/TestNo1.java
@@ -4,6 +4,12 @@ public class TestNo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Git Test");
+		System.out.println(numToSend());
+		
 	}
-
+	
+	private static int numToSend() {
+		int i = 13;
+		return i;
+	}
 }
