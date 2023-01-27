9835bea6603c41c39b993cdd012d6bceefe174d1
NEXT <NEXT@Merlin>
2023-01-27 19:50:06 +0700

    Commit test 01

---
+++ MerlinTestGit/src/gitTest/TestNo1.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+public class TestNo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Git Test");
+	}
+
+}
---
+++ MerlinTestGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NEXT
+ *
+ */
+module MerlinTestGit {
+}
